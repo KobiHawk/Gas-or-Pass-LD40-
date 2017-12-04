d7e4357c775fa2fa8ac26fd02a99fb534dca5b47
KobiHawk <kasaiaisu1@hotmail.com>
2017-12-04 03:17:16 -0600

    Added volume and toggle music functionality

---
+++ VolumeController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VolumeController : MonoBehaviour {
+
+    public AudioSource music;
+
+    private Slider slider;
+    private Toggle toggle;
+
+    private float musicStopTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        slider = GetComponent<Slider>();
+        toggle = GetComponentInParent<Toggle>();
+
+        slider.onValueChanged.AddListener(delegate { updateVolume(); });
+        toggle.onValueChanged.AddListener(delegate { updateToggle(); });
+
+        slider.value = .2f; // initial volume
+        musicStopTime = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void updateVolume()
+    {
+        music.volume = slider.value;
+    }
+
+    public void updateToggle()
+    {
+        if(toggle.isOn)
+        {
+            music.time = musicStopTime;
+            music.gameObject.SetActive(true);
+        }
+        else
+        {
+            musicStopTime = music.time;
+            music.gameObject.SetActive(false);
+        }
+    }
+
+}
